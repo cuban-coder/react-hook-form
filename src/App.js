0d196050d2b119e3ac1446190818ba80cb2aa0b3
cubancoder <wlunaj111@gmail.com>
2020-10-03 15:38:40 -0400

    agregando los input al estado entrada

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-import HookForm from './components/HookForm'
+//import HookForm from './components/HookForm'
+import Ejemplo from './components/Ejemplo'
 
 function App() {
   return (
     <div className="container">
-      <HookForm/>
+      {/* <HookForm/> */}
+      <Ejemplo/>
     </div>
   );
 }
---
+++ src/components/Ejemplo.js
@@ -0,0 +1,65 @@
+import React, { Fragment, useState } from 'react';
+
+import {useForm} from 'react-hook-form'
+
+const Ejemplo = () => {
+
+    const {register, errors, handleSubmit} = useForm();
+    const [entradas, setEntradas] = useState([])
+
+    const onSubmit = (data,e) => {
+        console.log(data)
+        setEntradas([
+            ...entradas,
+            data
+        ])
+        e.target.reset()
+    }
+
+    return ( 
+        <Fragment>
+            <h1>Ejemplo</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                 type="text"
+                 name= "titulo"
+                 placeholder="Ingrese el título"
+                 className="form-control my-2"
+                 ref = {
+                     register({
+                         required: {value: true, message: 'Campo obligatorio'},
+                         minLength: {value: 2, message: 'Mínimo dos letras'}
+                     })
+                 }
+                 />
+                 <span className="text-danger text-small d-block mb-2">
+                     {errors?.titulo?.message}
+                 </span>
+                 <input
+                 type="text"
+                 name= "description"
+                 placeholder="Ingrese la descripción"
+                 className="form-control my-2"
+                 ref = {
+                    register({
+                        required: {value: true, message: 'Campo obligatorio'},                       
+                    })
+                }
+                 />
+                 <span className="text-danger text-small d-block mb-2">
+                     {errors?.description?.message}
+                 </span>
+                 <button className="btn btn-primary">Agregar</button>
+            </form>
+            <ul>
+                {
+                entradas.map(entrada => 
+                <li>{entrada.titulo}-{entrada.description}</li>
+                )  
+                }
+            </ul>
+        </Fragment>
+     );
+}
+ 
+export default Ejemplo;
